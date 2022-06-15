95cf907430dc70e80a4d5c320b4be0ed4f19a075
RAFAIDEEN Richard <richard.rafaideen@gmail.com>
2022-06-15 16:53:55 +0200

    [refac] Move logger and static middleware in new library

--- app/Main.hs
+++ app/Main.hs
@@ -4,28 +4,7 @@ import Network.HTTP.Types (status404, status200)
 import Network.Wai.Handler.Warp (run)
 import Data.Text (isPrefixOf, pack, unpack)
 import System.Directory (doesFileExist)
-
--- | Print each request "[Method HTTP] - pathname"
-logger :: Middleware
-logger app request send = do
-  let path   = rawPathInfo request
-      method = requestMethod request
-  putStrLn "[Middleware] - My first middleware"
-  putStrLn $ "["++ show method ++"] - " ++ show path
-  app request send
-
-
--- | Serve static files
-static :: String -> String -> Middleware
-static routepath directory app request send = do
-  let info     = pathInfo request
-      path     = pack $ foldl (\acc x -> acc ++ "/" ++ unpack x) "" info
-      filepath = directory ++ drop (length routepath) (unpack path)
-      target   = if filepath == directory then directory ++ "/index.html" else filepath
-  exist <- doesFileExist target
-  if pack routepath `isPrefixOf` path && exist
-    then send $ responseFile status200 [] target Nothing
-    else app request send
+import Middleware.Common (logger)
 
 app :: Application
 app request send = do
@@ -37,4 +16,4 @@ app request send = do
 main = do
   putStrLn "Server started"
   putStrLn "http://localhost:3000"
-  run 3000 $ (logger {-  . static "/" "path/to/your/static/files" -}) app
+  run 3000 $ logger app
---
+++ lib/Middleware/Common.hs
@@ -0,0 +1,28 @@
+module Middleware.Common (logger, static) where 
+
+import Network.Wai
+import Network.HTTP.Types (status200)
+import Data.Text (isPrefixOf, pack, unpack)
+import System.Directory (doesFileExist)
+
+-- | Print each request "[Method HTTP] - pathname"
+logger :: Middleware
+logger app request send = do
+  let path   = rawPathInfo request
+      method = requestMethod request
+  putStrLn "[Middleware] - My first middleware"
+  putStrLn $ "["++ show method ++"] - " ++ show path
+  app request send
+
+
+-- | Serve static files
+static :: String -> String -> Middleware
+static routepath directory app request send = do
+  let info     = pathInfo request
+      path     = pack $ foldl (\acc x -> acc ++ "/" ++ unpack x) "" info
+      filepath = directory ++ drop (length routepath) (unpack path)
+      target   = if filepath == directory then directory ++ "/index.html" else filepath
+  exist <- doesFileExist target
+  if pack routepath `isPrefixOf` path && exist
+    then send $ responseFile status200 [] target Nothing
+    else app request send
